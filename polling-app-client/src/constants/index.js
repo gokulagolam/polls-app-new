3299c76ab00c693a49f7f08312b38941099c7e87
gokulagolam <122074192+gokulagolam@users.noreply.github.com>
2024-08-04 13:26:40 +0530

    Update index.js

--- polling-app-client/src/constants/index.js
+++ polling-app-client/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://44.204.104.43:8081/api';
 export const ACCESS_TOKEN = 'accessToken';
 
 export const POLL_LIST_SIZE = 30;
